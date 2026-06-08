9566d65f63d35ee5a8cd393fa63d7ff623367179
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-07 21:17:30 -0600

    Rework on Commit

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,24 +1,14 @@
 #include "grains.h"
-#include <math.h>
-uint64_t square(uint8_t index){
-    uint64_t total = (uint8_t)0;
-    
-    if(index > 0)
-    {
-        total = (uint64_t)pow(2, index - 1);
-    }
+#include <stdint.h>
 
-    return total; 
+uint64_t square(uint8_t index) {
+    if (index >= 1 && index <= 64) {
+        return 1ull << (index - 1);
+    }
+    
+    return 0;
 }
 
-uint64_t total(void){
-    uint64_t totalGrains = 0;
-    int squareIndex;
-
-    for(squareIndex = 0; squareIndex < 64; squareIndex++)
-    {
-        totalGrains += (uint64_t)pow(2, squareIndex);
-    }
-
-    return totalGrains;
+uint64_t total(void) {
+    return UINT64_MAX;
 }
