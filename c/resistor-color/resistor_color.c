84dd0d2d580150b273114b0e383d86766df8bea1
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-07 21:31:24 -0600

    Rework on Resistor Color

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,19 +1,16 @@
 #include "resistor_color.h"
-#include <stdlib.h>
-#include <string.h>
 
 int color_code(resistor_band_t color)
 {
-    return (int)color; 
+    return color; 
 }
 
 resistor_band_t *colors(void)
 {
-    const resistor_band_t color_names[] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+    static resistor_band_t color_names[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW, 
+        GREEN, BLUE, VIOLET, GREY, WHITE
+    };
     
-    resistor_band_t *colors_var = (resistor_band_t*)malloc(sizeof(color_names));
-    
-    memcpy(colors_var, color_names, sizeof(color_names));
-    
-    return colors_var;
+    return color_names;
 }
