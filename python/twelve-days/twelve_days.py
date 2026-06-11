00f0d9b6f35e76403ab8dc7888ebca799e1e8078
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-11 02:38:12 -0600

    Solve twelve-days in Python

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,27 @@
+LYRICS = {
+    1: ("first", "a Partridge in a Pear Tree."),
+    2: ("second", "two Turtle Doves, and "),
+    3: ("third", "three French Hens, "),
+    4: ("fourth", "four Calling Birds, "),
+    5: ("fifth", "five Gold Rings, "),
+    6: ("sixth", "six Geese-a-Laying, "),
+    7: ("seventh", "seven Swans-a-Swimming, "),
+    8: ("eighth", "eight Maids-a-Milking, "),
+    9: ("ninth", "nine Ladies Dancing, "),
+    10: ("tenth", "ten Lords-a-Leaping, "),
+    11: ("eleventh", "eleven Pipers Piping, "),
+    12: ("twelfth", "twelve Drummers Drumming, ")
+}
+
+def recite(start_verse, end_verse):
+    if start_verse <= 0 or end_verse <= 0 or start_verse > end_verse:
+        return
+        
+    result = []
+    for day_index in range(start_verse, end_verse + 1):
+        current_verse = "On the " + LYRICS[day_index][0] + " day of Christmas my true love gave to me: "
+        for gift_index in range(day_index, 0, -1):
+            current_verse += LYRICS[gift_index][1]
+        result.append(current_verse)
+
+    return result
