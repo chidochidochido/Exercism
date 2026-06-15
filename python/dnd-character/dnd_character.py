7a1f08352ea24a0d1a2afea389601d9f63c9cf1d
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-14 22:37:14 -0600

    python/dnd-character: implement Character and modifier using floor division and random

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,6 +1,18 @@
+import random
+
 class Character:
+    def ability(self):
+        rolls = [random.randint(1, 6) for _ in range(4)]
+        return sum(rolls) - min(rolls)
+        
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
 
 def modifier(value):
-    pass
+    return (value -10) // 2
