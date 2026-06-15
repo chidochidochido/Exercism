621999b0f641b62520d5830b36293afc3999a922
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-14 23:01:52 -0600

    python/dnd-character: address pylint conventions and add docstrings

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,8 +1,12 @@
+"""Module for generating DND character abilities and modifier calculations."""
 import random
 
-class Character:
-    def ability(self):
-        rolls = [random.randint(1, 6) for _ in range(4)]
+class Character: # pylint: disable=too-few-public-methods
+    """A class representing a D&D character with attributes and health."""
+    @staticmethod
+    def ability():
+        """Roll 4 six-sided dice and return the sum of the top 3 rolls."""
+        rolls = [random.randint(1, 6) for die_roll in range(4)]
         return sum(rolls) - min(rolls)
         
     def __init__(self):
@@ -15,4 +19,5 @@ class Character:
         self.hitpoints = 10 + modifier(self.constitution)
 
 def modifier(value):
+    """Calculate the ability modifier for a given score."""
     return (value -10) // 2
