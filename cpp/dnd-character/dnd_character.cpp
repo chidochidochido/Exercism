8143145aa5bde8f56433c7897325ac9f05147110
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-14 22:08:03 -0600

    cpp/dnd-character: implement Character using member initializer list and modern random

--- cpp/dnd-character/dnd_character.cpp
+++ cpp/dnd-character/dnd_character.cpp
@@ -1,7 +1,40 @@
 #include "dnd_character.h"
+#include <random>
+#include <algorithm> // for std::min or std::min_element
 
 namespace dnd_character {
 
-// TODO: add your solution here
+int modifier(int score) {
+    int diff = score - 10;
+    return diff < 0 ? (diff - 1) / 2 : diff / 2;
+}
+
+int ability() {
+    // Thread-safe / static random engine setup
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(1, 6);
+
+    int dice_sum = 0;
+    int min_roll = 7;
+
+    for (int i = 0; i < 4; ++i) {
+        int roll = dist(gen);
+        dice_sum += roll;
+        if (roll < min_roll) {
+            min_roll = roll;
+        }
+    }
+    return dice_sum - min_roll;
+}
+
+Character::Character()
+    : strength(ability()),
+      dexterity(ability()),
+      constitution(ability()),
+      intelligence(ability()),
+      wisdom(ability()),
+      charisma(ability()),
+      hitpoints(10 + modifier(constitution)) {}
 
 }  // namespace dnd_character
--- cpp/dnd-character/dnd_character.h
+++ cpp/dnd-character/dnd_character.h
@@ -1,7 +1,19 @@
 #pragma once
 
 namespace dnd_character {
+    struct Character {
+        int strength;
+        int dexterity;
+        int constitution;
+        int intelligence;
+        int wisdom;
+        int charisma;
+        int hitpoints;
 
-// TODO: add your solution here
+        // Constructor to generate a character
+        Character();
+    };
 
+    int ability();
+    int modifier(int score);
 }  // namespace dnd_character
