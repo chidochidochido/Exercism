b6caa2ae24f2159bcb532f53316af551edcfe492
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-18 13:34:07 -0600

    python/yacht: implement functional approach using lambda expressions

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,44 +1,19 @@
 """Module for scoring a Yacht dice game roll based on categories."""
 
-# Score categories.
-# Change the values as you see fit.
-YACHT = 0
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
-FULL_HOUSE = 7
-FOUR_OF_A_KIND = 8
-LITTLE_STRAIGHT = 9
-BIG_STRAIGHT = 10
-CHOICE = 11
+YACHT = lambda dice: 50 if len(set(dice)) == 1 else 0
+ONES = lambda dice: dice.count(1) * 1
+TWOS = lambda dice: dice.count(2) * 2
+THREES = lambda dice: dice.count(3) * 3
+FOURS = lambda dice: dice.count(4) * 4
+FIVES = lambda dice: dice.count(5) * 5
+SIXES = lambda dice: dice.count(6) * 6
+FULL_HOUSE = lambda dice: sum(dice) if sorted(dice.count(die) for die in set(dice)) == [2, 3] else 0
+FOUR_OF_A_KIND = lambda dice: next((die * 4 for die in set(dice) if dice.count(die) >= 4), 0)
+LITTLE_STRAIGHT = lambda dice: 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
+BIG_STRAIGHT = lambda dice: 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
+CHOICE = sum
+
 
 def score(dice, category):
     """Calculate the score of a 5-dice roll for a given category."""
-    if category == YACHT:
-        return 50 if len(set(dice)) == 1 else 0
-
-    if ONES <= category <= SIXES:
-        return dice.count(category) * category
-
-    if category == FULL_HOUSE:
-        return sum(dice) if sorted(dice.count(die) for die in set(dice)) == [2, 3] else 0
-
-    if category == CHOICE:
-        return sum(dice)
-
-    if category == FOUR_OF_A_KIND:
-        for die in set(dice):
-            if dice.count(die) >= 4:
-                return die * 4
-        return 0
-
-    if category == LITTLE_STRAIGHT:
-        return 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
-
-    if category == BIG_STRAIGHT:
-        return 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
-
-    return 0
+    return category(dice)
