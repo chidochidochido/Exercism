0578f1cf65ee44f5663ab8e750ab506b3768ef38
Ramirez Marco (UID87429) <Marco.2.Ramirez@continental-corporation.com>
2026-06-18 12:49:53 -0600

    python/yacht: implement score function using imperative approach with standard methods

---
+++ python/yacht/yacht.py
@@ -0,0 +1,42 @@
+# Score categories.
+# Change the values as you see fit.
+YACHT = 0
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = 7
+FOUR_OF_A_KIND = 8
+LITTLE_STRAIGHT = 9
+BIG_STRAIGHT = 10
+CHOICE = 11
+
+
+
+def score(dice, category):
+    if category == YACHT:
+        return 50 if len(set(dice)) == 1 else 0
+
+    if ONES <= category <= SIXES:
+        return dice.count(category) * category
+
+    if category == FULL_HOUSE:
+        return sum(dice) if sorted(dice.count(x) for x in set(dice)) == [2, 3] else 0
+    
+    if category == CHOICE:
+        return sum(dice)
+
+    if category == FOUR_OF_A_KIND:
+        # We can iterate unique values in set(dice)
+        for x in set(dice):
+            if dice.count(x) >= 4:
+                return x * 4
+        return 0
+
+    if category == LITTLE_STRAIGHT:
+        return 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
+
+    if category == BIG_STRAIGHT:
+        return 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
